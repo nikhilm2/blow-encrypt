2ffdfdf5721ddf79cfd5c2a074b47bcd715217ec
nikhilm2 <nikhil.paul211@gmail.com>
2024-06-06 10:13:32 +0530

    added Team.jsx

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,29 +1,32 @@
+// components/Header.js
 "use client"
-import { Button } from '@/components/ui/button'
-import { SignInButton, UserButton, useUser } from '@clerk/nextjs'
-import Image from 'next/image'
-import React from 'react'
-import Link from 'next/link'
+import React, { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import { SignInButton, UserButton, useUser } from '@clerk/nextjs';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
-    const {user, isSignedIn} = useUser();
+  const { user, isSignedIn } = useUser();
   return (
     <div className='p-5 border-b shadow-sm'>
-        <div className='flex items-center justify-between '>
-        <Link href={"/"}><Image src={"/logo.svg"} 
-        width={50} height={50} alt='logo'/></Link>
-        {
-            isSignedIn? <div className='flex gap-2'>
-                <Link href={"/dashboard"}><Button >Dashboard</Button></Link>
-                <UserButton/>
-            </div>: <SignInButton>
-            <Button>Get Started</Button>
-            </SignInButton>
-        }
-       
-        </div>
-    </div>
-  )
-}
+      <div className='flex items-center justify-between '>
+        <Link href={"/"}><Image src={"/logo.svg"} width={50} height={50} alt='logo' /></Link>
 
-export default Header
+        <div className='flex gap-2'>
+          <Link href={"/team"}><Button variant="ghost">Team</Button></Link>
+          {isSignedIn ? (
+            <>
+              <Link href={"/dashboard"}><Button>Dashboard</Button></Link>
+              <UserButton />
+            </>
+          ) : (
+            <SignInButton><Button>Get Started</Button></SignInButton>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
